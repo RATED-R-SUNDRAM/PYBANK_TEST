012b63a6f970979d259129a6de6bd8dc70abfa45
RATED-R-SUNDRAM <ss6437p@gmail.com>
2026-03-31 09:31:57 +0530

    addedd some code

---
+++ pybank/account.py
@@ -0,0 +1,22 @@
+class Account:
+    def __init__(self, owner: str, balance: float = 0.0):
+        self.owner = owner
+        self._balance = balance
+        self._history = []
+
+    def deposit(self, amount: float) -> float:
+        if amount <= 0:
+            raise ValueError("Deposit must be positive")
+        self._balance += amount
+        self._history.append(("deposit", amount))
+        return self._balance
+
+    def withdraw(self, amount: float) -> float:
+        if amount > self._balance:
+            raise ValueError("Insufficient funds")
+        self._balance -= amount
+        self._history.append(("withdraw", amount))
+        return self._balance
+
+    def balance(self) -> float:
+        return self._balance
